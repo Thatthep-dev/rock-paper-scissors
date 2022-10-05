aa10af92f3823ee938f79f651a3d0771d598ca6d
Thatthep <thatthep.dev@gmail.com>
2022-10-05 21:09:25 +0700

    Add playRound & game function, Complete version 1.0.0

--- index.js
+++ index.js
@@ -9,14 +9,48 @@ function getComputerChoice() {
 }
 
 function playerSelection() {
-    playerChoice = prompt('Please selected Rock, Paper, Scissors') ?? 'Please Select';
+    playerChoice = prompt('Please selected Rock, Paper, Scissors');
     playerChoice = playerChoice.toLowerCase();
     return playerChoice
 }
 
 // Game logic is here
 function playRound() {
+    getComputerChoice();
+    playerSelection();
     switch (true) {
-        case playerChoice = 
+        case playerChoice === comChoice:
+            result = 'draw';
+            return result
+        case playerChoice === 'rock' && comChoice === 'scissors':
+            result = 'win';
+            return result
+        case playerChoice === 'rock' && comChoice === 'paper':
+            result = 'lose';
+            return result
+        case playerChoice === 'paper' && comChoice === 'rock':
+            result = 'win';
+            return result
+        case playerChoice === 'paper' && comChoice === 'scissors':
+            result = 'lose';
+            return result
+        case playerChoice === 'scissors' && comChoice === 'paper':
+            result = 'win';
+            return result
+        case playerChoice === 'scissors' && comChoice === 'rock':
+            result = 'lose';
+            return result
+        default:
+            result = "We don't have that choice.";
+            return result
+        }
+}
+
+function game() {
+    for (i=0; i < 5; i++) {
+        playRound();
+        console.log(comChoice);
+        console.log(playerChoice);
+        console.log(result);
     }
 }
